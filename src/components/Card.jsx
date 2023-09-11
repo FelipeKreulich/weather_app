758e5e5ee8abdce3fa1a2b506864b27ed33efcfe
FelipeKreulich <felipek2002k@gmail.com>
2023-09-11 14:43:32 -0400

    Create base e Component Card

---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <div>
+      <div>
+        <span>Campo Grande</span>
+        <span>Mato Grosso do Sul, Brasil</span>
+      </div>
+
+      <div>
+        <spanpan>Temperatura</spanpan>
+        <span>°C</span>
+      </div>
+
+      <div>
+        <span>icone</span>
+        <span>Nublado</span>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
